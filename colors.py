6d0666e792cf72a284753cdf79432c8f4916b11d
John Weachock <jweachock@gmail.com>
2013-05-26 21:21:54 -0400

    Updated to a much prettier output format.

--- colors.py
+++ colors.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
+import sys
 
 # highlights a string with the given color
-def highlight(fg = None, bg = None):
+def highlight(text = ' *** ', fg = None, bg = None):
 	# adjust the fg color to the correct escape code
 	if fg != None:
 		if fg > 7:
@@ -16,35 +17,63 @@ def highlight(fg = None, bg = None):
 		else:
 			bg_esc = bg + 40
 
-	if bg == None: # print a fg and blank bg
-		return '\033[%dm %2d/   \033[0m' % (fg_esc, fg)
+	if bg is None: # print a fg and blank bg
+		return '\033[%dm%s\033[0m' % (fg_esc, text)
 
-	if fg == None: # print a bg as a fg
-		return '\033[%dm   /%2d \033[0m' % (bg_esc - 10, bg)
+	if fg is None: # print a bg as a fg
+		return '\033[%dm%s\033[0m' % (bg_esc - 10, text)
 
 	# print a fg and bg
-	return '\033[%dm\033[%dm %2d/%2d \033[0m' % (fg_esc, bg_esc, fg, bg)
+	return '\033[%dm\033[%dm%s\033[0m' % (fg_esc, bg_esc, text)
 
-def print_palette(offset = 0):
-	# header
-	row = '       '
-	for b in range(8):
-		row += highlight(bg = b + offset)
-	print row
+def print_short():
+	print ''
 
-	# palette
-	for f in range(16):
-		row = ''
-		row += highlight(fg = f)
+	for f in range(3):
+		row = '  '
 		for b in range(8):
-			row += highlight(fg = f, bg = b + offset)
+			row += highlight(text='    ', fg = b, bg = b)
+			row += highlight(text='    ', fg = b + 8, bg = b + 8)
+			row += '  '
 		print row
 
 	print ''
 
-def main():
-	print_palette(0)
-	print_palette(8)
+def print_palette():
+	print ''
+
+	row = ' '*5
+	for b in range(8):
+		row += highlight(text=' %2s ' % b, fg = b)
+		row += highlight(text=' %2s ' % (b + 8), fg = b + 8)
+		row += '  '
+	print row
+
+	row = ' '*5
+	for b in range(8):
+		row += highlight(text='    ', fg = b, bg = b)
+		row += highlight(text='    ', fg = b + 8, bg = b + 8)
+		row += '  '
+	print row
+
+	for f in range(16):
+		row = highlight(text=' %2s  ' % f, fg = f)
+
+		for b in range(8):
+			row += highlight(text=' %2s ' % f, fg = f, bg = b)
+			row += highlight(text=' %2s ' % f, fg = f, bg = b + 8)
+			row += '  '
+		print row
+
+	print ''
+
+def main(args):
+	if len(args) == 0:
+		print_palette()
+	elif args[0] in ('-s', '--short'):
+		print_short()
+	else:
+		print 'Usage: ./colors.py [-s|--short]'
 
 if __name__ == '__main__':
-	main()
+	main(sys.argv[1:])
